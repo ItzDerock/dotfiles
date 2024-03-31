84951777eec042eb6fc1c7d19c29574902bb4fff
Derock <derock@derock.dev>
2024-03-31 11:41:49 -0400

    feat: devenv

--- flake.nix
+++ flake.nix
@@ -6,12 +6,15 @@
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
       "https://cuda-maintainers.cachix.org"
+      "https://devenv.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" 
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
+
+    trustedUsers = [ "root" "@wheel" ];
   };
 
   inputs = {
