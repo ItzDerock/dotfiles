74fcc2576fbb27c1868a7aba98d190415cc22dac
Derock <derock@derock.dev>
2024-04-06 16:44:42 -0400

    fix: switch port for primary monitor

--- home-manager/overrides/supernova.nix
+++ home-manager/overrides/supernova.nix
@@ -1,20 +1,23 @@
 { ... }:
+let
+  MAIN_DISPLAY = "DP-2";
+in 
 {
   programs.waybar.settings.mainBar = {
-    output = "DP-3"; 
+    output = MAIN_DISPLAY; 
   };
 
   wayland.windowManager.hyprland.settings = {
     # monitors
     monitor = [
       "HDMI-A-1,1920x1080@60,-1920x0,1" # random cheapass 1080p
-      "DP-3,3440x1440@180,0x0,1" # asus ultrawide
+      "DP-2,3440x1440@180,0x0,1" # asus ultrawide
       "DP-1,3840x2160@60,3440x0,1" # 4k 
     ];
   
-    # make DP-3 always wksp 1
+    # make main always wksp 1
     workspace = [
-      "1,monitor:DP-3,default:true,persistent:true"
+      "1,monitor:${MAIN_DISPLAY},default:true,persistent:true"
       "name:screen-2,monitor:HDMI-A-1,default:true,persistent:true"
       "name:screen-3,monitor:DP-1,default:true,persistent:true"
     ];
