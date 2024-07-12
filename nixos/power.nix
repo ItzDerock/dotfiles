28b856e55bd6fab0c491c7d816394aab61e99f48
Derock <derock@derock.dev>
2024-07-11 23:17:53 -0400

    fix: power

--- nixos/power.nix
+++ nixos/power.nix
@@ -15,8 +15,13 @@ in {
       settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+	CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        # CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
+        PLATFORM_PROFILE_ON_BAT = "low-power";
+        PLATFORM_PROFILE_ON_AC = "performance";
+        RUNTIME_PM_ON_AC = "auto";
+        WIFI_PWR_ON_BAT = "on";
       };
     };
   };
