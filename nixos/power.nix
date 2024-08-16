2345a04c4f047da4437b8b479241efce383296ac
Derock <derock@derock.dev>
2024-08-16 08:26:15 -0400

    fix: power

--- nixos/power.nix
+++ nixos/power.nix
@@ -9,32 +9,36 @@ in {
 
   config = mkIf cfg.enable { 
     powerManagement.enable = true;
-    services.thermald.enable = false; # borken
+    services.thermald.enable = true; # borken
     services.tlp = {
-      enable = false;
+      enable = true;
       settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-	      # CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+	# CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
         # CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
         PLATFORM_PROFILE_ON_BAT = "low-power";
         PLATFORM_PROFILE_ON_AC = "performance";
         RUNTIME_PM_ON_AC = "auto";
+	RUNTIME_PM_ON_BAT = "auto";
         WIFI_PWR_ON_BAT = "on";
+	WIFI_PWR_ON_AC = "on";
+	NMI_WATCHDOG = "1";
       };
     };
 
-		services.auto-cpufreq.enable = true;
-		services.auto-cpufreq.settings = {
-			battery = {
-				 governor = "powersave";
-				 turbo = "never";
-			};
-			charger = {
-				 governor = "performance";
-				 turbo = "auto";
-			};
-		};
+    services.auto-cpufreq.enable = true;
+    services.auto-cpufreq.settings = {
+      battery = {
+        governor = "powersave";
+	turbo = "auto";
+      };
+      charger = {
+        governor = "performance";
+	turbo = "auto";
+      };
+    };
+
+    # prevent docker from auto-starting
+    systemd.services.docker.wantedBy = lib.mkForce [];
   };
 }
