e6c055a0f413f6e9e94c09d2f8b05045d523b29d
Derock <derock@derock.dev>
2025-07-12 22:50:35 -0400

    fix: remove hardcoded displays

--- home-manager/overrides/supernova.nix
+++ home-manager/overrides/supernova.nix
@@ -3,17 +3,17 @@ let
   MAIN_DISPLAY = "DP-2";
 in 
 {
-  programs.waybar.settings.mainBar = {
-    output = MAIN_DISPLAY; 
-  };
+  # programs.waybar.settings.mainBar = {
+  #   output = MAIN_DISPLAY; 
+  # };
 
   wayland.windowManager.hyprland.settings = {
     # monitors
-    monitor = [
-      "HDMI-A-1,1920x1080@60,-1920x0,1" # random cheapass 1080p
-      "DP-2,3440x1440@180,0x0,1" # asus ultrawide
-      "DP-3,3840x2160@60,3440x0,1" # 4k 
-    ];
+    # monitor = [
+    #   "HDMI-A-1,1920x1080@60,-1920x0,1" # random cheapass 1080p
+    #   "DP-1,3440x1440@180,0x0,1" # asus ultrawide
+    #   "DP-2,3840x2160@60,3440x0,1" # 4k 
+    # ];
 
     env = [
       # disable VRR, should fix flickering
