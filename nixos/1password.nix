32e5e4eefbba94fcb74b3e038978f08b503a6d28
Derock <derock@derock.dev>
2025-11-26 15:38:58 -0500

    fix: use 1password beta

--- nixos/1password.nix
+++ nixos/1password.nix
@@ -7,11 +7,13 @@ in {
     enable = mkEnableOption "1password";
   };
 
-  config = mkIf cfg.enable { 
+  config = mkIf cfg.enable {
     programs._1password.enable = true;
     programs._1password-gui = {
       enable = true;
       polkitPolicyOwners = ["derock"];
+      # https://github.com/hyprwm/Hyprland/discussions/11978
+      package = pkgs._1password-gui-beta;
     };
 
     environment.etc = {
