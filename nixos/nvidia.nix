78004a6f48f466d15cadaf120be2a1ff746b6811
Derock <derock@derock.dev>
2026-06-20 17:56:03 +0900

    feat: update nvidia to compile for 7.1

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -61,12 +61,12 @@ in
       # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
       # Only available from driver 515.43.04+
       # Currently alpha-quality/buggy, so false is currently the recommended setting.
-      open = false;
+      open = true;
       nvidiaSettings = true;
 
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
       # Stable has a use-after-free bug .-.
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
 
       prime = mkIf (!cfg.primary) {
         intelBusId = "PCI:0:2:0";
